52df68c6ea312047addfabd7ddaac04757eaa42f
CapnSquirrel <azapataa@g.ucla.edu>
2021-03-15 00:19:32 -0700

    asset dump and scene rough layout

--- project.js
+++ project.js
@@ -13,13 +13,22 @@ export class Project extends Scene {
 
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
-            cube: new Cube(),
-            tube: new defs.Cylindrical_Tube(1, 10, [[0, 2], [0, 1]]),
             sphere: new defs.Subdivision_Sphere(4),
-            picker_planet: new (defs.Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
-            apple_tree: new Shape_From_File("assets/Apple_Tree.obj"),
-//             apple_tree: new Shape_From_File("assets/desk_chair_scene.obj"),
-            huge_scene: new Shape_From_File("assets/huge_scene.obj"),
+            apple_tree: new Shape_From_File("assets/objs/apple_tree.obj"),
+            apple: new Shape_From_File("assets/objs/apple.obj"),
+            bed: new Shape_From_File("assets/objs/bed.obj"),
+            closet: new Shape_From_File("assets/objs/closet.obj"),
+            desk: new Shape_From_File("assets/objs/desk.obj"),
+            desk_chair: new Shape_From_File("assets/objs/desk_chair.obj"),
+            door: new Shape_From_File("assets/objs/door.obj"),
+            table: new Shape_From_File("assets/objs/table.obj"),
+            floor: new Shape_From_File("assets/objs/floor.obj"),
+            grass: new Shape_From_File("assets/objs/grass.obj"),
+            shelf: new Shape_From_File("assets/objs/shelf.obj"),
+            sofa: new Shape_From_File("assets/objs/sofa.obj"),
+            walls: new Shape_From_File("assets/objs/walls.obj"),
+            window: new Shape_From_File("assets/objs/window.obj"),
+
         };
 
         // *** Materials
@@ -31,11 +40,76 @@ export class Project extends Scene {
                 ambient: .5, diffusivity: 0.1, specularity: 0.1,
                 texture: new Texture("assets/test_sky.png") // texture by LateNighCoffe on itch.io
             }),
-            apple_tree_texture: new Material(new Textured_Phong(), {
+            apple_tree: new Material(new Textured_Phong(), {
                 color: hex_color("#ffffff"),
                 ambient: .3, diffusivity: 0.1, specularity: 0.1,
-                texture: new Texture("assets/Tree Texture.png")
-            })
+                texture: new Texture("assets/textures/apple_tree.png")
+            }),
+            apple: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/apple.png"),
+            }),
+            bed: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/bed.png"),
+            }),
+            closet: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/closet.png"),
+            }),            
+            desk: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/desk.png"),
+            }),
+            desk_chair: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/desk_chair.png"),
+            }),
+            door: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/door.png"),
+            }),
+            table: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/table.png"),
+            }),
+            floor: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/floor.png"),
+            }),
+            grass: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/grass.png"),
+            }),
+            shelf: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/shelf.png"),
+            }),
+            sofa: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/sofa.png"),
+            }),
+            walls: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/walls.png"),
+            }),                                                                                    
+            window: new Material(new Textured_Phong(), {
+                color: hex_color("#ffffff"),
+                ambient: .3, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/textures/window.png"),
+            }),
         }
         
         this.shapes.sphere.arrays.texture_coord.forEach(p => p.scale_by(4));
@@ -43,11 +117,50 @@ export class Project extends Scene {
     }
 
     make_control_panel() {
-        // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-        this.key_triggered_button("Default camera.", ["Control", "0"], () => this.attached = () => this.initial_camera_location);
-        this.new_line();
-        this.key_triggered_button("Look at object.", ["Control", "0"], () => this.attached = () => this.object_1);
-        this.new_line();
+
+
+    }
+
+    drawStuff(context, program_state) {
+                
+        let apple_tree = Mat4.identity();
+        this.shapes.apple_tree.draw(context, program_state, apple_tree, this.materials.apple_tree);
+
+        let bed = Mat4.identity();
+        this.shapes.bed.draw(context, program_state, bed, this.materials.bed);
+
+        let closet = Mat4.identity();
+        this.shapes.closet.draw(context, program_state, closet, this.materials.closet);
+
+        let desk = Mat4.identity();
+        this.shapes.desk.draw(context, program_state, desk, this.materials.desk);
+
+        let desk_chair = Mat4.identity();
+        this.shapes.desk_chair.draw(context, program_state, desk_chair, this.materials.desk_chair);
+
+        let door = Mat4.identity();
+        this.shapes.door.draw(context, program_state, door, this.materials.door);
+
+        let table = Mat4.identity();
+        this.shapes.table.draw(context, program_state, table, this.materials.table);
+
+        let floor = Mat4.identity();
+        this.shapes.floor.draw(context, program_state, floor, this.materials.floor);
+
+        let grass = Mat4.identity();
+        this.shapes.grass.draw(context, program_state, grass, this.materials.grass);
+
+        let shelf = Mat4.identity();
+        this.shapes.shelf.draw(context, program_state, shelf, this.materials.shelf);       
+
+        let sofa = Mat4.identity();
+        this.shapes.sofa.draw(context, program_state, sofa, this.materials.sofa);
+
+        let walls = Mat4.identity();
+        this.shapes.walls.draw(context, program_state, walls, this.materials.walls);
+
+        let window = Mat4.identity();
+        this.shapes.window.draw(context, program_state, window, this.materials.window);                                                                                         
 
     }
 
@@ -88,14 +201,27 @@ export class Project extends Scene {
 
         this.make_sky_box(context, program_state, t);
 
-        let flat_plane = Mat4.identity().times(Mat4.scale(100, 1/20, 100)).times(Mat4.translation(0, -20, 0));
-        this.shapes.cube.draw(context, program_state, flat_plane, this.materials.test.override({color: hex_color("#00ff00")}));
+//         let flat_plane = Mat4.identity().times(Mat4.scale(100, 1/20, 100)).times(Mat4.translation(0, -20, 0));
+//         this.shapes.cube.draw(context, program_state, flat_plane, this.materials.test.override({color: hex_color("#00ff00")}));
 
 //         let apple_tree_model = Mat4.identity();
-//         this.shapes.apple_tree.draw(context, program_state, apple_tree_model, this.materials.apple_tree_texture);
+//         this.shapes.apple_tree.draw(context, program_state, apple_tree_model, this.materials.apple_tree);
 
-        let huge_scene_transform = Mat4.identity();
-        this.shapes.huge_scene.draw(context, program_state, huge_scene_transform, this.materials.apple_tree_texture);
+//         let apple = Mat4.identity();
+//         this.shapes.apple.draw(context, program_state, apple, this.materials.apple);
+
+//         let desk = Mat4.identity();
+//         this.shapes.desk.draw(context, program_state, desk, this.materials.desk);
+
+//         let desk_chair = Mat4.identity();
+//         this.shapes.desk_chair.draw(context, program_state, desk_chair, this.materials.desk_chair);
+        
+//         let window = Mat4.identity();
+//         this.shapes.window.draw(context, program_state, window, this.materials.window);
+        
+        this.drawStuff(context, program_state);
+//         let huge_scene_transform = Mat4.identity();
+//         this.shapes.huge_scene.draw(context, program_state, huge_scene_transform, this.materials.apple_tree_texture);
         
     }
 }
